65db513c66d698d76315141ca9a3f84fb6bf71ba
Alexwork93 <ortwinkrausemail@gmail.com>
2021-12-13 16:37:45 +0200

    my commit

--- 01_Basics/Instance_11_arrays/script.js
+++ 01_Basics/Instance_11_arrays/script.js
@@ -27,7 +27,7 @@ console.log(arrExample);    //[ 6, 7, 8, 9, 10, 11, 12, 13, 65 ]
 console.log('\n\n\n');
 
 for (let value of arrExample) {
-    if(arrExample.indexOf(value) > 5){
+    if (arrExample.indexOf(value) > 5) {
         continue;
     }
     arrExample[arrExample.indexOf(value)] += 10;
@@ -49,3 +49,69 @@ arrExample.forEach(function (value, index, array) {
     mess += `Our array after changes ${array}\n`;
     console.log(mess);
 });
+
+
+// string.split(", "); returns array of values from the string. To clear work of our code we have to pass appropriate type of splitter
+
+const someString = "AAA, asdasd, dadasfas, erfarfa, fasdfasdf, asdfasdf, gwtgwert, hrthdtghst, gtawerfsdfa, asergfasdfa";
+
+const arrayFromString = someString.split(", ");
+
+console.log(arrayFromString);
+//[ 'AAA',
+//   'asdasd',
+//   'dadasfas',
+//   'erfarfa',
+//   'fasdfasdf',
+//   'asdfasdf',
+//   'gwtgwert',
+//   'hrthdtghst',
+//   'gtawerfsdfa',
+//   'asergfasdfa' ]
+
+// array.join('splitter'); returns string from array values with specified splitter
+
+const newStringFromArray = arrayFromString.join(" <superSplitter> ");
+
+console.log(newStringFromArray);
+//AAA <superSplitter> asdasd <superSplitter> dadasfas <superSplitter> erfarfa <superSplitter> fasdfasdf <superSplitter> asdfasdf <superSplitter> gwtgwert <superSplitter> hrthdtghst <superSplitter> gtawerfsdfa <superSplitter> asergfasdfa
+
+
+// Sorting of array elements
+
+arrayFromString.sort();
+console.log(arrayFromString);
+//[ 'AAA',
+//   'asdasd',
+//   'asdfasdf',
+//   'asergfasdfa',
+//   'dadasfas',
+//   'erfarfa',
+//   'fasdfasdf',
+//   'gtawerfsdfa',
+//   'gwtgwert',
+//   'hrthdtghst' ]
+
+// But it doesn't work with numbers
+// To sort array with numbers we have to modify our comparing method
+
+const unsortedNumbersArray = [123, 43, 23, 5, 1324, 6, 54, 234, 6, 786, 12, 43, 41, 13, 434];
+
+const sortedNumbersArray = unsortedNumbersArray.sort(function (a, b) {
+    return a - b;
+})
+
+console.log(sortedNumbersArray);
+//[ 5, 6, 6, 12, 13, 23, 41, 43, 43, 54, 123, 234, 434, 786, 1324 ]
+
+
+
+
+
+
+
+
+
+
+
+
