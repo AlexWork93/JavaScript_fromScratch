dba2f6230525725fd8ffda6c106f5b15b5d04fe7
Alexwork93 <ortwinkrausemail@gmail.com>
2021-12-16 15:03:40 +0200

    my commit

---
+++ 02_complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,94 @@
+//Coding Challenge #1
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+
+
+// const Mark = {
+//     weight: 78,
+//     height: 1.69,
+//     calculateBMI: function () {
+//         const bmi = this.weight / (this.height ** 2);
+//         console.log(bmi);
+//         return bmi;
+//     }
+// }
+//
+// const John = {
+//     weight: 95,
+//     height: 1.88,
+//     calculateBMI: function () {
+//         const bmi = this.weight / (this.height ** 2);
+//         console.log(bmi);
+//         return bmi;
+//     }
+// }
+//
+// // const Mark = {
+// //     weight: 95,
+// //     height: 1.88,
+// //     calculateBMI: function (){
+// //         const bmi = this.weight / (this.height ** 2);
+// //         console.log(bmi);
+// //         return bmi;
+// //     }
+// // }
+// //
+// // const John = {
+// //     weight: 85,
+// //     height: 1.76,
+// //     calculateBMI: function (){
+// //         const bmi = this.weight / (this.height ** 2);
+// //         console.log(bmi);
+// //         return bmi;
+// //     }
+// // }
+//
+// const markHigherBMI = Mark.calculateBMI() > John.calculateBMI();
+// console.log(markHigherBMI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
