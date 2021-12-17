91745916788c9c3e43844e71252a680852c0115f
Alexwork93 <ortwinkrausemail@gmail.com>
2021-12-17 17:16:38 +0200

    my commit

--- 02_complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ 02_complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -35,7 +35,7 @@ const calcAverage = score => {
         res += score[key];
         counter++;
     }
-    return res/counter;
+    return res / counter;
 }
 
 const dolphins = {
@@ -47,11 +47,11 @@ const koalas = {
 }
 
 function checkWinner(avgDolphins, avgKoalas) {
-    if (avgDolphins >= avgKoalas * 2){
+    if (avgDolphins >= avgKoalas * 2) {
         return `Dolphins win ${avgDolphins} : ${avgKoalas}`;
-    }else if (avgKoalas >= avgDolphins * 2){
+    } else if (avgKoalas >= avgDolphins * 2) {
         return `Koalas win ${avgKoalas} : ${avgDolphins}`;
-    }else {
+    } else {
         return `We do not have any winner`;
     }
 }
@@ -63,6 +63,67 @@ console.log(checkWinner(calcAverage(dolphins["score"]), calcAverage(koalas["scor
 dolphins.score = [85, 54, 41];
 koalas.score = [23, 34, 27];
 console.log(checkWinner(calcAverage(dolphins["score"]), calcAverage(koalas["score"])));
+
+
+//Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) 😉
+
+const tips = [],
+    bills = [125, 555, 44],
+    total = [],
+    tipRate = [];
+
+function calcTip(bill, key) {
+    if (bill > 50 && bill < 300) {
+        tipRate[key] = '15%';
+        return bill * 15 / 100;
+    } else {
+        tipRate[key] = '20%';
+        return bill * 20 / 100;
+    }
+}
+
+for (const billsKey in bills) {
+    tips[billsKey] = calcTip(bills[billsKey], billsKey);
+    total[billsKey] = tips[billsKey] + bills[billsKey];
+}
+
+for (const tipsKey in tips) {
+    console.log(`Bill ${bills[tipsKey]} \ntip ${tips[tipsKey]} \ntip rate ${tipRate[tipsKey]} \ntotal prise ${total[tipsKey]}\n`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
