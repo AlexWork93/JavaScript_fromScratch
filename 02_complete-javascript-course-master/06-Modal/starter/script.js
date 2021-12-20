195b7c8836098359ae708421a918886030a0b438
Alexwork93 <ortwinkrausemail@gmail.com>
2021-12-20 18:25:01 +0200

    my commit

--- 02_complete-javascript-course-master/06-Modal/starter/script.js
+++ 02_complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < showModalBtns.length; i++) {
+    showModalBtns[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+});
+
+
+function openModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+
+
+
+
+
+
+
+
+
--- 02_complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ 02_complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+const playerFirst = {
+    score: document.querySelector('#score--0'),
+
+
+};
+
+const playerSecond = {
+
+};
