8e5ee3ab2ab330e2cee04783884e293463d2b091
Alexwork93 <ortwinkrausemail@gmail.com>
2021-12-21 18:39:10 +0200

    my commit

--- 02_complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ 02_complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+
+function calcAge(birthday) {
+    const age = 2021 - birthday;
+    function printAge() {
+        const mess = `I am ${firstName} and i am ${age}`;
+        console.log(mess);
+    }
+    printAge();
+    return (age);
+}
+
+
+const firstName = 'Alex';
+console.log(calcAge(1993));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
